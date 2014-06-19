b825b8c2d5ea125073fc3ecc2d7fb9a1b4b390da
LiZhen <dao.yi.er.san@gmail.com>
2014-06-19 16:40:02 +0800

    Finish connect front and back with jQuery
    Using JSON

--- DBFinalExp/DBFinalExp/urls.py
+++ DBFinalExp/DBFinalExp/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-from DBFinalExp.views import home
+from DBFinalExp import views
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -15,5 +15,6 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
-	url(r'^$', home),
+	url(r'^$', views.home),
+	url(r'^search/$', views.search),
 )
--- DBFinalExp/DBFinalExp/views.py
+++ DBFinalExp/DBFinalExp/views.py
@@ -2,59 +2,74 @@
 
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
-import pickle
+#import pickle
 from django.views.decorators.csrf import csrf_exempt
+from django.utils import simplejson
 import os
+import re
 
 def home(request):
-    return render_to_response('home.html')
+	return render_to_response('home.html')
 
 @csrf_exempt
 def search(request):
-    
-    para = str(request.POST['SearchWord']).strip().lower()
-    if para == '':
-        return render_to_response('home.html')
-    findex = open('indexpickle.txt', 'rb')
-    indexfile = pickle.load(findex)
-    findex.close()
-    
-    retlist = []
-    num = 0
-        
-    paralist = para.split()
-    isOK = True
-    searchlist = []
-    
-    for word in paralist:
-        if indexfile.has_key(word):
-            searchlist += [indexfile[word]]
-        else:
-            isOK = False
-            break
-        
-    if isOK:
-        def reduceFunc(x, y):
-            return list(set(x) & set(y))
+	if 'keyWords' in request.POST:
+		keyWords = request.POST['keyWords']
+		# Get latitude and longitude from Google Latlng format.
+		latlng = re.findall(r'[-]?[\d]*\.[\d]*', request.POST['userPos'])
+		userLat = float(latlng[0])
+		userLng = float(latlng[1])
+		rtnMsg = [{"Lat":-30, "Lng":140}, {"Lat":-50, "Lng":160}, {"Lat":-70, "Lng":100}]	
+		#rtnMsg = "Lat:" + str(userLat) + " ; Lng:" + str(userLng) + " ; KeyWords:" + keyWords
+	else:
+		rtnMsg = "Wrong Keywords"
+	rtnMsgJSON = simplejson.dumps(rtnMsg, ensure_ascii=False)
+	print rtnMsgJSON
+	return HttpResponse(rtnMsgJSON)
+	#return HttpResponse(rtnMsg)
+	# para = str(request.POST['SearchWord']).strip().lower()
+	# if para == '':
+		# return render_to_response('home.html')
+	# findex = open('indexpickle.txt', 'rb')
+	# indexfile = pickle.load(findex)
+	# findex.close()
+	
+	# retlist = []
+	# num = 0
+		
+	# paralist = para.split()
+	# isOK = True
+	# searchlist = []
+	
+	# for word in paralist:
+		# if indexfile.has_key(word):
+			# searchlist += [indexfile[word]]
+		# else:
+			# isOK = False
+			# break
+		
+	# if isOK:
+		# def reduceFunc(x, y):
+			# return list(set(x) & set(y))
 
-        searchlist = reduce(reduceFunc, searchlist)
-        num += len(searchlist)
-        for item in searchlist:
-            f3 = open(r'%s\profInfos\%d.txt' % (os.getcwd(), int(item)), 'r')
-            profInfo = f3.readlines()
-            f3.close()
-            
-            f4 = open(r'%s\profPlainText\%d.txt' % (os.getcwd(), int(item)), 'r')
-            profText = f4.read()
-            f4.close()
-            pos = profText.find(paralist[0])
-            abstract = ''
-            if pos >= 0:
-                st = max(0, pos - 40)
-                ed = min(pos + 60, len(profText))
-                abstract = '%s<font color=#FF0000>%s</font>%s' % (profText[st:pos], paralist[0], profText[pos+len(paralist[0]):ed])
-            
-            retlist += [{'name':profInfo[0].strip(), 'url':profInfo[1].strip(), 'text':abstract}]
-            print profInfo[0].strip(), profInfo[1].strip()
-            
-    return render_to_response('search.html', {'keywords':para, 'num':num, 'profDics':retlist})
+		# searchlist = reduce(reduceFunc, searchlist)
+		# num += len(searchlist)
+		# for item in searchlist:
+			# f3 = open(r'%s\profInfos\%d.txt' % (os.getcwd(), int(item)), 'r')
+			# profInfo = f3.readlines()
+			# f3.close()
+			
+			# f4 = open(r'%s\profPlainText\%d.txt' % (os.getcwd(), int(item)), 'r')
+			# profText = f4.read()
+			# f4.close()
+			# pos = profText.find(paralist[0])
+			# abstract = ''
+			# if pos >= 0:
+				# st = max(0, pos - 40)
+				# ed = min(pos + 60, len(profText))
+				# abstract = '%s<font color=#FF0000>%s</font>%s' % (profText[st:pos], paralist[0], profText[pos+len(paralist[0]):ed])
+			
+			# retlist += [{'name':profInfo[0].strip(), 'url':profInfo[1].strip(), 'text':abstract}]
+			# print profInfo[0].strip(), profInfo[1].strip()
+			
+	# return render_to_response('search.html', {'keywords':para, 'num':num, 'profDics':retlist})
